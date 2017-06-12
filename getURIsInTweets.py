786286c3f1cbcfc03b87e13885597878c2ae8170
Mat Kelly <mkelly@cs.odu.edu>
2017-06-12 15:48:19 +0000

    Static srcs

--- getURIsInTweets.py
+++ getURIsInTweets.py
@@ -1,11 +1,12 @@
 import json
 import gzip
 import sys
-
+import glob
 # Reads all files, supplied by args, breaks into py objs
 
 def main():
-  tweetGZs = sys.argv[1:]  
+  tweetGZs = glob.glob("/mnt/intersect/tweets/*.json.gz")
+
   tweets = []
   for gzs in tweetGZs:
     with gzip.open(gzs, 'r') as gz:
@@ -14,13 +15,13 @@ def main():
 
 # [{u'indices': [42, 62], u'url': u'http://t.co/uogjb7YJ', u'expanded_url': u'http://bibliosol.wordpress.com', u'display_url': u'bibliosol.wordpress.com'}, {u'indices': [63, 83], u'url': u'http://t.co/uogjb7YJ', u'expanded_url': u'http://bibliosol.wordpress.com', u'display_url': u'bibliosol.wordpress.com'}]
 
-  print "expandeduri,uri,createdAt"
-  for tweetRaw in tweets:
-    tweet = json.loads(tweetRaw)
-    if 'entities' in tweet and 'urls' in tweet['entities']:
-      for url in tweet['entities']['urls']:
-        print "{0}\t{1}\t{2}\t{3}".format(url['expanded_url'],url['url'],tweet['created_at'],tweet['id'])
-
+      out = "expandeduri,uri,createdAt\n"
+      for tweetRaw in tweets:
+        tweet = json.loads(tweetRaw)
+        if 'entities' in tweet and 'urls' in tweet['entities']:
+          for url in tweet['entities']['urls']:
+            out += "{0}\t{1}\t{2}\t{3}\n".format(url['expanded_url'],url['url'],tweet['created_at'],tweet['id'])
+     print out
 
 if __name__ == "__main__":
   main()
